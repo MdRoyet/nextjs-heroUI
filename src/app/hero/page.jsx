111f55734050af0a3069abfbb855c34d8c8fa6d9
Md Royet <rroyet8@gmail.com>
2026-04-23 00:16:02 +0600

    heroUI install and button added

---
+++ src/app/hero/page.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@heroui/react";
+
+const HeroPage = () => {
+  return (
+    <div>
+      <h2>Hero Page</h2>
+      <Button variant="primary">Hero Button</Button>
+    </div>
+  );
+};
+
+export default HeroPage;
